7d9271c6ae94abb2711f5ca567aaf9e0725ee86b
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-05-01 21:52:38 +0530

    Add project files.

---
+++ ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntroductiontoModelValidationsCoreMVC6.Models;
+
+namespace IntroductiontoModelValidationsCoreMVC6.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("register")]
+    public IActionResult Index(Person person)
+    {
+      return Content($"{person}");
+    }
+  }
+}
